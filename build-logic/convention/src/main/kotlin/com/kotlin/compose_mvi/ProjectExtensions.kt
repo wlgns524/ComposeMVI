cd9abadb51b33d5d0dc7ea60c6f2cb50f4db4c82
Android-Jihoon <jh.park@fanmaum.com>
2023-11-12 18:14:25 +0900

    Refactor location

--- build-logic/convention/src/main/kotlin/com/kotlin/compose_mvi/ProjectExtensions.kt
+++
@@ -1,9 +0,0 @@
-package com.kotlin.compose_mvi
-
-import org.gradle.api.Project
-import org.gradle.api.artifacts.VersionCatalog
-import org.gradle.api.artifacts.VersionCatalogsExtension
-import org.gradle.kotlin.dsl.getByType
-
-val Project.libs
-    get(): VersionCatalog = extensions.getByType<VersionCatalogsExtension>().named("libs")
